bd1147082bc515a03f77759bfcb3d6504bc84ff4
John Hill <john@proofpositiveimaging.com>
2016-07-27 11:19:11 -0400

    working animation

--- browser/js/item.controller.js
+++ browser/js/item.controller.js
@@ -1,44 +1,45 @@
 'use strict'
 
-DateApp.controller('dateCtrl', function($scope){
-  console.log('in controller');
+DateApp.controller('dateCtrl', function($scope, $timeout){
   $scope.animateClass = "";
+  var animateCounter = 0;
   var today = new Date().toDateString();
   $scope.startDate = new Date(today);
   $scope.endDate = new Date(today);
 
-
-
   $scope.calcMyDays = function (){
-    // var start = $scope.startDate;
-    // var end =  $scope.endDate;
+    animateCounter ++;
     var start = new Date($scope.startDate);
     var end = new Date($scope.endDate);
-
     var diff =  Math.abs(end-start);
     var diffCalc = ((((diff/1000)/60)/60)/24);
     $scope.days = diffCalc;
-    // $scope.animateClass = "usePulse";
+    if (animateCounter > 2) {
+      $scope.animateClass = "usePulse";
+      $timeout(function removeAnimate(){
+        $scope.animateClass = "";
+      }, 1000);
+    }
   };
 
-  $scope.$watch('endDate', function(){
+  $scope.$watch('endDate', function (newValue, oldValue, scope) {
     $scope.calcMyDays();
-  });
+}, true);
+
   $scope.$watch('startDate', function(){
     $scope.calcMyDays();
   });
+
   $scope.enterFunction = function(){
     var start = new Date($scope.startDate).toDateString();
     var end = new Date(start);
     var moreDays = Number($scope.days);
     end.setDate(end.getDate() + moreDays);
     $scope.endDate = new Date(end);
-    $scope.animateClass = "usePulse";
-
   };
+
   $scope.showEnterButton = function(){
     $scope.showButton = true;
   };
-
 
 });//end controller
--- browser/js/module.js
+++ browser/js/module.js
@@ -1,4 +1,3 @@
 'use strict';
 
 var DateApp = angular.module('DateApp', ['ngMaterial']);
-console.log('inside here');
